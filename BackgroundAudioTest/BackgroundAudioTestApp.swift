e922ad3312b0db7077df1e719463425917f24260
swifticul <swifticul@icloud.com>
2023-02-18 14:13:19 +0100

    Initial Commit

---
+++ BackgroundAudioTest/BackgroundAudioTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BackgroundAudioTestApp.swift
+//  BackgroundAudioTest
+//
+//  Created by Theo De Roy on 18/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct BackgroundAudioTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
